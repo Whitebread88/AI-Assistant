9335f493e00ca1b09b3b6029a47ab8f26f287f44
AaronFoong <foongpro@gmail.com>
2026-02-19 11:56:56 +0800

    Create storage.py

---
+++ storage.py
@@ -0,0 +1,11 @@
+from google.cloud import storage
+import os
+
+BUCKET_NAME = os.environ.get("KNOWLEDGE_BUCKET")
+
+storage_client = storage.Client()
+
+def load_category_file(category: str) -> str:
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blob = bucket.blob(f"knowledge/{category}.txt")
+    return blob.download_as_text()
