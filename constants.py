ca1c530ca2fa99fc040bdfdb999b6593128e2a24
AaronFoong <foongpro@gmail.com>
2026-02-27 14:28:58 +0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,1 +1,1 @@
-CATEGORIES = ["Contact", "Experience", "Education", "Project", "Skills", "Personal_Information", "Ambition", "Collaboration", "Hobby", "Value", "AI_Assistant", "Summary"]
+CATEGORIES = ["Contact", "Experience", "Education", "Project", "Skills", "Personal_Information", "Ambition", "Collaboration", "Hobby", "Value", "AI_Assistant", "Summary", "Weakness"]
